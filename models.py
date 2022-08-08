9e23851b4c909514d9eb6d1e52b8e569f59012cd
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-08 18:23:09 -0500

    Created and added data to models.py file

--- models.py
+++ models.py
@@ -6,6 +6,20 @@ db = SQLAlchemy()
 
 
 def create_app():
-    app =Fl
+    app = Flask(__name__)
+    app.config.from_object(devConfig())
     db.init_app(app)
     return app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), nullable=False, primary_key=True, unique=True)
+    score = db.Column(db.Float(), nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username):
+        self.username = username
+        self.score = 0.0
+
+    def __repr__(self):
+        return '<User %r>' % self.username
