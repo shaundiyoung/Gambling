94dffb9977afa6b89e7e94f7f55057fc274a9422
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-08 17:08:38 -0500

    Set the configurations in the config file

---
+++ config.py
@@ -0,0 +1,12 @@
+
+class config(object):
+    SECRET_KEY = "81346y5028560834yv08we4yt082480u5y043986y0834y6vhdfugjghszjkdfbi$%^&&HVBFOIUBgiodvubVBKUBVPIOUHBPiouhHIUHHWPhbpipunsipbndgv"
+
+class prodConfig(config):
+    DATABASE_URI = 'To be decided'
+
+class devConfig(config):
+    DATABASE_URI = "sqlite:///temp/foo.db"
+
+class testingConfig(config):
+    DATABASE_URI = 'sqlite:///:memory:'
