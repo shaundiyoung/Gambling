19c7c614faf5322936f33ff32b3fc2dd749f962b
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-08 23:09:37 -0500

    update to the index.html file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from models import create_app, db
+from flask import render_template
 
 app = create_app()
 
@@ -6,6 +7,10 @@ app = create_app()
 @app.route('/')
 def home():
     return "<h1>Hello World</h1>"
+
+@app.route('/landing')
+def landing():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
