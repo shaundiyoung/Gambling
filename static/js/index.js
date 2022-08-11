51b4ed2cc8281998ef9ee8499bf31c9e4fa8652c
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-11 12:11:18 -0500

    Added sound effects to the number change

--- static/js/index.js
+++ static/js/index.js
@@ -2,11 +2,13 @@ num = document.getElementById('guess');
 num.innerText = 13;
 let finishDate;
 let startDate = new Date().getMinutes();
+let x = document.getElementById("myAudio"); 
 
 function randomNum(){
     num.innerText = Math.floor(Math.random() * 33);
-    setTimeout(randomNum, 300);
+    setTimeout(randomNum, 100);
     console.log(startDate);
+    x.play();
 }
 
 function initiallizeCount(){
