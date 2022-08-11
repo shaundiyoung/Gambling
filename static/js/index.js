b894d79df95e86ca60e7fd252255c7cf6f427f02
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-11 07:43:30 -0500

    Add how to play section

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,14 @@
 num = document.getElementById('guess');
 num.innerText = 13;
+let finishDate;
+let startDate = new Date().getMinutes();
 
 function randomNum(){
     num.innerText = Math.floor(Math.random() * 33);
     setTimeout(randomNum, 300);
-    
+    console.log(startDate);
+}
+
+function initiallizeCount(){
+    finishDate = startDate+1;
 }
