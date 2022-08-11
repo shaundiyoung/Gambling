79dc2afda89b5b194f076e482e2205691d734553
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-11 15:26:14 -0500

    Added How to Play Instructions

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,22 @@
 num = document.getElementById('guess');
 num.innerText = "00";
-let finishDate;
+let finishDate = 0;
 let startDate = new Date().getMinutes();
 let x = document.getElementById("myAudio"); 
+let cash = document.getElementById('bal');
 
 function randomNum(){
+    if (finishDate >= 10000){
+        finishDate = 0;
+    }
+    else{
+        
     num.innerText = Math.floor(Math.random() * 33);
     setTimeout(randomNum, 100);
     console.log(startDate);
     x.play();
+    finishDate += 100;
+    }
 }
 
 function initiallizeCount(){
