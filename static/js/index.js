82469cf791e26b4c6ce6c4e751269e5598a5e537
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-12 08:58:27 -0500

    Validate the Data to the range

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,13 @@
 num = document.getElementById('cashpot');
 num.innerText = "00";
 let finishDate = 0;
-let startDate = new Date().getMinutes();
+let randNum = 0 ;
 let x = document.getElementById("myAudio"); 
+
+let y = document.getElementById("myAudio1"); 
+
+let z = document.getElementById("myAudio2"); 
+
 let cash = document.getElementById('bal');
 let val = document.getElementById('val');
 let guess = document.getElementById('guess');
@@ -16,25 +21,34 @@ val.addEventListener('mouseout', () =>{
     document.getElementById('Profit').innerText = 0+val.value*2;
 });
 function randomNum(){
+    let guessnum = guess.value;
     if (finishDate >= 7000){
         finishDate = 0;
+        randNum = num.innerText;
+        console.log(randNum);
+        checknumber(randNum, guessnum);
     }
     else{
         
     num.innerText = Math.floor(Math.random() * 33);
     setTimeout(randomNum, 100);
-    console.log(startDate);
     x.play();
     finishDate += 100;
     }
 }
 
-function initiallizeCount(){
-    finishDate = startDate+1;
-}
 
+function checknumber(num, guessnum){
+    if (num == guessnum){
+        alert("You won");
+        z.play();
+    }
+    else if (num-guessnum>-4 && num-guessnum<4){
+        alert("Close enough");
+        z.play();
+    }
+    else{
+        y.play();
 
-function submitVal(){
-    let guessnum = guess.value;
-    console.log(guessnum);
+    }
 }
