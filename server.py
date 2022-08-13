fba21c5cdeaf2cf547c9bb969586cb2a5eb4e94b
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2022-08-12 19:49:03 -0500

    Added Flask messages to the login Form

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from models import create_app, db
-from flask import render_template, url_for, flash, request
+from flask import render_template, url_for, flash, request, redirect, session
 from myforms import myForm, signUp
 
 app = create_app()
@@ -8,6 +8,9 @@ app = create_app()
 @app.route('/', methods=["GET", "POST"])
 def home():
     form = signUp()
+    if form.validate_on_submit():
+        flash(form.password.data)
+        # return redirect('landing')
     return render_template("login.html", form=form)
 
 @app.route('/landing')
